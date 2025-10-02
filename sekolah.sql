7541e05935e9917e9c93b573100ff4aecbe52304
Evan <evanmarhian2@gmail.com>
2025-10-02 20:27:28 +0700

    Membuat table siswa dan nilai

--- sekolah.sql
+++ sekolah.sql
@@ -1,2 +1,35 @@
 CREATE DATABASE sekolah;
 USE sekolah;
+
+-- public.siswa definition
+
+-- Drop table
+
+-- DROP TABLE siswa;
+
+CREATE TABLE siswa (
+	id serial4 NOT NULL,
+	nama varchar NOT NULL,
+	umur int4 NULL,
+	jurusan varchar NULL,
+	CONSTRAINT siswa_pkey PRIMARY KEY (id)
+);
+
+-- public.nilai definition
+
+-- Drop table
+
+-- DROP TABLE nilai;
+
+CREATE TABLE nilai (
+	id serial4 NOT NULL,
+	siswa_id serial4 NOT NULL,
+	mata_pelajaran varchar NULL,
+	nilai int4 NULL,
+	CONSTRAINT nilai_pkey PRIMARY KEY (id)
+);
+
+
+-- public.nilai foreign keys
+
+ALTER TABLE public.nilai ADD CONSTRAINT nilai_siswa_id_fkey FOREIGN KEY (siswa_id) REFERENCES siswa(id);
